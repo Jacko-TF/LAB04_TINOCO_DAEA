c9a4a32f08a9eefa45b2ac60f8aa09fdf16f9669
Alumno <Alumno@LAB1504-15>
2024-04-06 12:51:08 -0500

    Agregar archivos de proyecto.

---
+++ LAB04_TINOCO_DAEA/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using LAB04_TINOCO_DAEA.models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB04_TINOCO_DAEA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Data Source=LAB1504-15\\SQLEXPRESS;Initial Catalog=NeptunoDB;User Id=jacko;Password=admin123";
+
+            List <Producto> productos = new List<Producto>();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("USP_ListProducts", connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32("idproducto");
+                    string nom =  reader.GetString("nombreProducto");
+                    string cant = reader.GetString("cantidadPorUnidad");
+                    decimal pre = reader.GetDecimal("precioUnidad");
+                    string cat;
+                    if (!reader.IsDBNull("categoriaProducto"))
+                    {
+                        cat = reader.GetString("categoriaProducto");
+                    }
+                    else
+                    {
+                        cat = null;
+                    }
+
+                    productos.Add(new Producto(id, nom, cant, pre, cat));
+                }
+
+                connection.Close();
+
+                dgProductos.ItemsSource = productos;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Data Source=LAB1504-15\\SQLEXPRESS;Initial Catalog=NeptunoDB;User Id=jacko;Password=admin123";
+
+            List<Categoria> categorias = new List<Categoria>();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("USP_ListCategories", connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32("idcategoria");
+                    string nom = reader.GetString("nombrecategoria");
+                    string des = reader.GetString("descripcion");
+                    bool act = reader.GetBoolean("Activo");
+                    string cod = reader.GetString("CodCategoria");
+
+                    categorias.Add(new Categoria(id, nom, des, act, cod));
+                }
+
+                connection.Close();
+
+                dgCategorias.ItemsSource = categorias;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ LAB04_TINOCO_DAEA/models/Categoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB04_TINOCO_DAEA.models
+{
+    public class Categoria
+    {
+        public int idcategoria { get; set; }
+        public string nombrecategoria { get; set; }
+
+        public string descripcion { get; set;}
+
+        public bool activo { get; set; }
+
+        public string CodCategoria { get; set;}
+
+        public Categoria(int id, string nom, string des, bool act, string cod)
+        {
+            this.idcategoria = id;
+            this.nombrecategoria = nom;
+            this.descripcion = des;
+            this.activo = act;
+            this.CodCategoria = cod;
+        }
+    }
+}
